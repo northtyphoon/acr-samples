bef2296a26c99f237185812d4712d134b7d6ec5a
Bin Du <bindu@microsoft.com>
2025-12-27 21:24:08 -0800

    update script

--- acr-purge-in-aci/run-acr-with-mi.sh
+++ acr-purge-in-aci/run-acr-with-mi.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
+# Install jq for JSON parsing
 tdnf install -y jq
 
-aad_access_token=$(curl 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https://containerregistry.azure.net/' -H Metadata:true | jq -r '.access_token')
-acr_refresh_token=$(curl -v -X POST -H "Content-Type: application/x-www-form-urlencoded" -d "grant_type=access_token&service=$Registry&tenant=$Tenant&access_token=$aad_access_token" https://$Registry/oauth2/exchange)
-$acr_refresh_token | acr login $Registry -u 00000000-0000-0000-0000-000000000000 --password-stdin
+# Obtain AAD access token from the managed identity
+aad_access_token=$(curl -H Metadata:true 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https://containerregistry.azure.net/' | jq -r '.access_token')
+
+# Exchange AAD access token for ACR refresh token and login
+curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d "grant_type=access_token&service=$Registry&tenant=$Tenant&access_token=$aad_access_token" https://$Registry/oauth2/exchange \
+ | jq -r '.refresh_token' \
+ | acr login $Registry -u 00000000-0000-0000-0000-000000000000 --password-stdin
 
 acr "$@"
